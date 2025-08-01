a0f8bdf2969063a15749ecbfd6246bd20e3feddc
BigOrangeQWQ <2284086963@qq.com>
2025-08-01 13:38:56 +0800

    fix: OneOf type JSON-Related methods

--- test/go-gen/cli/main.go
+++ test/go-gen/cli/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -35,35 +36,52 @@ func marshal_data_write_to_file(outputDir, filename string, data []byte) error {
 	return nil
 }
 
-func marshal_and_write_test_case(outputDir, name string, testCase proto.Message) {
-	options := proto.MarshalOptions{
-		Deterministic: true,
-	}
-	data, err := options.Marshal(testCase)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error marshaling %s: %v\n", name, err)
-		return
-	}
-	if err := marshal_data_write_to_file(outputDir, name, data); err != nil {
-		fmt.Fprintf(os.Stderr, "Error generating %s: %v\n", name, err)
-		os.Exit(1)
+func marshal_and_write_test_case(outputDir, name string, testCase proto.Message, format string) {
+	if format == "json" {
+		data, err := protojson.Marshal(testCase)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error marshaling %s to JSON: %v\n", name, err)
+			return
+		}
+		outputPath := filepath.Join(outputDir, fmt.Sprintf("%s.json", name))
+		if err := write_to_file(outputPath, data); err != nil {
+			fmt.Fprintf(os.Stderr, "Error writing JSON %s: %v\n", name, err)
+			os.Exit(1)
+		}
+	} else {
+		options := proto.MarshalOptions{
+			Deterministic: true,
+		}
+		data, err := options.Marshal(testCase)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error marshaling %s: %v\n", name, err)
+			return
+		}
+		if err := marshal_data_write_to_file(outputDir, name, data); err != nil {
+			fmt.Fprintf(os.Stderr, "Error generating %s: %v\n", name, err)
+			os.Exit(1)
+		}
 	}
 }
 
 func main() {
-	var outputDir = flag.String("o", "./bin", "Output directory for generated binary files")
-	flag.StringVar(outputDir, "output", "./bin", "Output directory for generated binary files")
+	var outputDir = flag.String("o", "./bin", "Output directory for generated files")
+	flag.StringVar(outputDir, "output", "./bin", "Output directory for generated files")
+	var format = flag.String("f", "bin", "Output format: bin or json")
+	flag.StringVar(format, "format", "bin", "Output format: bin or json")
 	var help = flag.Bool("help", false, "Show help message")
 
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS]\n\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, "Generate protobuf test case binary files.\n\n")
+		fmt.Fprintf(os.Stderr, "Generate protobuf test case files.\n\n")
 		fmt.Fprintf(os.Stderr, "Options:\n")
 		flag.PrintDefaults()
 		fmt.Fprintf(os.Stderr, "\nExamples:\n")
-		fmt.Fprintf(os.Stderr, "  %s                           # Generate files to ./bin\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, "  %s -o ./test-data            # Generate files to ./test-data\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, "  %s --output /tmp/protobuf    # Generate files to /tmp/protobuf\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s                           # Generate bin files to ./bin\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s -o ./test-data            # Generate bin files to ./test-data\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s --output /tmp/protobuf    # Generate bin files to /tmp/protobuf\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s -f json                   # Generate json files to ./bin\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s --format json             # Generate json files to ./bin\n", os.Args[0])
 	}
 
 	flag.Parse()
@@ -74,13 +92,21 @@ func main() {
 
 	for i, testCase := range p3testCases {
 		name := fmt.Sprintf("proto3_test_case_%d", i+1)
-		marshal_and_write_test_case(*outputDir, name, testCase)
+		marshal_and_write_test_case(*outputDir, name, testCase, *format)
 	}
 	for i, testCase := range p2testCases {
 		name := fmt.Sprintf("proto2_test_case_%d", i+1)
-		marshal_and_write_test_case(*outputDir, name, testCase)
+		marshal_and_write_test_case(*outputDir, name, testCase, *format)
 	}
 
 	name := "proto3_empty"
-	write_to_file(filepath.Join(*outputDir, fmt.Sprintf("%s.bin", name)), emptyTestCase)
+	if *format == "json" {
+		outputPath := filepath.Join(*outputDir, fmt.Sprintf("%s.json", name))
+		if err := write_to_file(outputPath, []byte("{}")); err != nil {
+			fmt.Fprintf(os.Stderr, "Error writing JSON %s: %v\n", name, err)
+			os.Exit(1)
+		}
+	} else {
+		write_to_file(filepath.Join(*outputDir, fmt.Sprintf("%s.bin", name)), emptyTestCase)
+	}
 }
